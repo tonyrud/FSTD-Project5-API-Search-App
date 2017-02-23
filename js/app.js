ba02330b5a17d6982bac304898284b3e757506cf
tonyrud <tonrudny@gmail.com>
2017-02-23 18:46:01 -0500

    testing completed

--- js/app.js
+++ js/app.js
@@ -4,10 +4,7 @@
         $albums = $('#albums'),
         $message = $('.desc'),
         albumDetails = [],
-        clickedIndex,
-        albumClicked = false;
-
-
+        clickedIndex;
 
     $('form').submit(function (e) {
         e.preventDefault();
@@ -15,6 +12,7 @@
         let searchedText = $searchField.val(),
             noAlbums = 'No albums found that match: ',
             emptySearch = '<p>Nothing in search field!</p>',
+            //add message function with default params
             addMessage = (icon = 'help', html = noAlbums, message = searchedText) => {
                 return `<li class='no-albums'>
                     <i class='material-icons icon-help'>${icon}_outline</i>${html} <strong>${message}</strong>.
@@ -40,26 +38,25 @@
     $albums.on('click', '.album', function (e) {
         e.preventDefault();
 
-        let imageSelected = $(this).find('.album-artist'),
-            clickedIndex = $(this).index();
+        let clickedIndex = $(this).index();
 
+        let album = albumDetails[clickedIndex];
 
-        generateAlbumDetails(clickedIndex);
+        ajaxTracksCall(album.id)
+            .then(generateAlbumDetails);
     });
 
     $("body").on('click', '.back-link', function (e) {
         e.preventDefault();
         $(this).parent().parent().fadeOut(400);
-
-
     });
 
     /*check if there are albums from the searched input,
     return nothing to throw catch error in promise*/
     let checkData = (data) => !data.albums.items.length ? "" : data;
 
-    let ajaxCall = function (query) {
-        return new Promise(function (resolve, reject) {
+    let ajaxCall = query => {
+        return new Promise((resolve, reject) => {
             $.ajax({
                 url: 'https://api.spotify.com/v1/search',
                 data: {
@@ -71,14 +68,25 @@
         });
     };
 
-    let generateGallery = (data) => {
+    //Ajax call for track info
+    let ajaxTracksCall = item => {
+        // let imageId = album.id;
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                url: 'https://api.spotify.com/v1/albums/' + item,
+                success: resolve
+            });
+        })
+    };
+
+    let generateGallery = data => {
         let albums = data.albums.items,
             htmlString = "";
         //clear album array    
         albumDetails = [];
 
-        albums.forEach((album) => {
-
+        albums.forEach(album => {
+            //add current album to album array
             albumDetails.push(album);
 
             htmlString += `
@@ -99,37 +107,25 @@
         return albumDetails;
     }
 
-    let generateAlbumDetails = (index) => {
-        let album = albumDetails[index],
-            htmlString = "";
-
-        albumClicked = true;
-
+    let generateAlbumDetails = album => {
+        let htmlString = "";
         htmlString += `
         <section id='overlay'>
             <div class="overlay-content">
             <a href='#' class="back-link">
-                <p class="back-search"><i class="material-icons">arrow_back</i><span>Search Results</span></p>
+                <i class="material-icons">arrow_back</i><span>Search Results</span>
             </a>
             <div class="album-info">
-
                 <figure class="album-cover">
                     <img src="${album.images[0].url}" alt="">
                 </figure>
                 <div class="album-details">
-                <h1>${album.name}(YEAR)</h1>
+                <h1>${album.name} (${album.release_date.slice(0, 4)})</h1>
                 <a  href="${album.external_urls.spotify}"  target="_blank">
                     <p class="ext-link"><i class="material-icons">link</i><span>${album.artists[0].name}</span></p>
                 </a>
                 <ul class="tracks">
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
-                    <li class="track">Track</li>
+                ${createTracks(album.tracks.items)}
                 </ul>
                 </div>
             </div>
@@ -138,27 +134,39 @@
         appendAlbumDetails(htmlString);
     }
 
-    let appendItems = (content) => {
-        //remove animation class to animate out
-        $('#albums > li').removeClass('album-animate');
-        //wait, then add everything
-        setTimeout(() => {
-            //clear albums
-            $albums.empty();
-            //add albums
-            $albums.append(content);
-            //animate albums in
-            $('#albums > li').each(function (i) {
-                var $item = $(this);
-                setTimeout(function () {
-                    $item.addClass('album-animate');
-                }, 100 * i);
+    let createTracks = tracks => {
+        let trackString = '';
+        tracks.forEach((track) => {
+            trackString += `
+                <li class="track">${track.track_number} ${track.name}</li>
+                `
             });
-        }, 400);
-    }
+            return trackString;
+        };
 
-    let appendAlbumDetails = (content) => {
-        //add albums
-        $('.main-content').after(content);
-    };
-})(jQuery);
+        let appendItems = content => {
+            //remove animation class to animate out
+            $('#albums > li').removeClass('album-animate');
+            //wait, then add everything
+            setTimeout(() => {
+                //clear albums
+                $albums.empty();
+                $('#overlay').fadeOut(200);
+                //add albums
+                $albums.append(content);
+                //animate albums in
+                $('#albums > li').each(function (i) {
+                    var $item = $(this);
+                    setTimeout(function () {
+                        $item.addClass('album-animate');
+                    }, 100 * i);
+                });
+            }, 400);
+        };
+
+        let appendAlbumDetails = content => {
+            //add album detail page
+            $('#overlay').remove();
+            $('.main-content').append(content).hide().fadeIn(400);
+        };
+    })(jQuery);
